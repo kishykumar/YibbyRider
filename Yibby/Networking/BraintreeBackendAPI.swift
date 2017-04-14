65c6e1f41d5ccebbe79e45d245193d1e34712bc2
Rahul Mehndiratta <rahulmehndiratta@live.com>
2017-04-15 04:19:45 +0530

    update card
    Former-commit-id: 3e6e1a7c702176b4025f0c94599aea549d01c91b

--- Yibby/Networking/BraintreeBackendAPI.swift
+++ Yibby/Networking/BraintreeBackendAPI.swift
@@ -32,6 +32,9 @@ public protocol BraintreeBackendAPIAdapter {
     func updateSourceForCustomer(_ paymentMethod: BTPaymentMethodNonce,
                                  oldPaymentMethod: String,
                                  completion: @escaping BTErrorBlock)
+    func updateSourceForCustomerString(_ paymentMethod: String,
+                                 oldPaymentMethod: String,
+                                 completion: @escaping BTErrorBlock)
     
     func attachSourceToCustomer(_ paymentMethod: BTPaymentMethodNonce, completion: @escaping BTErrorBlock)
     
@@ -138,7 +141,7 @@ class BraintreeBackendAPI: NSObject, BraintreeBackendAPIAdapter {
     func fetchClientToken(_ completionBlock: @escaping TokenFetchCompletionBlock) {
         
         let client: BAAClient = BAAClient.shared()
-        guard let customerID = customerID else {
+        guard customerID != nil else {
             completionBlock("eyJ2ZXJzaW9uIjoyLCJhdXRob3JpemF0aW9uRmluZ2VycHJpbnQiOiI2YmU3MGIzY2IzNTcwZDY3MzU0YjdjMmIyMGQyNjllOGM5ODkwNTE5MGYxZGRlN2ZkN2Q5NDIzY2U3MmQ0ZTAwfGNyZWF0ZWRfYXQ9MjAxNi0wOC0wN1QwOTowNzowOC41NDgyMzUxNjErMDAwMFx1MDAyNm1lcmNoYW50X2lkPWRjcHNweTJicndkanIzcW5cdTAwMjZwdWJsaWNfa2V5PTl3d3J6cWszdnIzdDRuYzgiLCJjb25maWdVcmwiOiJodHRwczovL2FwaS5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tOjQ0My9tZXJjaGFudHMvZGNwc3B5MmJyd2RqcjNxbi9jbGllbnRfYXBpL3YxL2NvbmZpZ3VyYXRpb24iLCJjaGFsbGVuZ2VzIjpbXSwiZW52aXJvbm1lbnQiOiJzYW5kYm94IiwiY2xpZW50QXBpVXJsIjoiaHR0cHM6Ly9hcGkuc2FuZGJveC5icmFpbnRyZWVnYXRld2F5LmNvbTo0NDMvbWVyY2hhbnRzL2RjcHNweTJicndkanIzcW4vY2xpZW50X2FwaSIsImFzc2V0c1VybCI6Imh0dHBzOi8vYXNzZXRzLmJyYWludHJlZWdhdGV3YXkuY29tIiwiYXV0aFVybCI6Imh0dHBzOi8vYXV0aC52ZW5tby5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tIiwiYW5hbHl0aWNzIjp7InVybCI6Imh0dHBzOi8vY2xpZW50LWFuYWx5dGljcy5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tL2RjcHNweTJicndkanIzcW4ifSwidGhyZWVEU2VjdXJlRW5hYmxlZCI6dHJ1ZSwicGF5cGFsRW5hYmxlZCI6dHJ1ZSwicGF5cGFsIjp7ImRpc3BsYXlOYW1lIjoiQWNtZSBXaWRnZXRzLCBMdGQuIChTYW5kYm94KSIsImNsaWVudElkIjpudWxsLCJwcml2YWN5VXJsIjoiaHR0cDovL2V4YW1wbGUuY29tL3BwIiwidXNlckFncmVlbWVudFVybCI6Imh0dHA6Ly9leGFtcGxlLmNvbS90b3MiLCJiYXNlVXJsIjoiaHR0cHM6Ly9hc3NldHMuYnJhaW50cmVlZ2F0ZXdheS5jb20iLCJhc3NldHNVcmwiOiJodHRwczovL2NoZWNrb3V0LnBheXBhbC5jb20iLCJkaXJlY3RCYXNlVXJsIjpudWxsLCJhbGxvd0h0dHAiOnRydWUsImVudmlyb25tZW50Tm9OZXR3b3JrIjp0cnVlLCJlbnZpcm9ubWVudCI6Im9mZmxpbmUiLCJ1bnZldHRlZE1lcmNoYW50IjpmYWxzZSwiYnJhaW50cmVlQ2xpZW50SWQiOiJtYXN0ZXJjbGllbnQzIiwiYmlsbGluZ0FncmVlbWVudHNFbmFibGVkIjp0cnVlLCJtZXJjaGFudEFjY291bnRJZCI6InN0Y2gybmZkZndzenl0dzUiLCJjdXJyZW5jeUlzb0NvZGUiOiJVU0QifSwiY29pbmJhc2VFbmFibGVkIjp0cnVlLCJjb2luYmFzZSI6eyJjbGllbnRJZCI6IjdlYTc5N2EyYmY2ZjM2YmY5NjFmYTc5ZTk0YTQwNjBlODM2ZTc1NmEyYzM1ZGU4MjlmYzM0NzI3YTJhYmYxODEiLCJtZXJjaGFudEFjY291bnQiOiJjb2luYmFzZS1zYW5kYm94LXNoYXJlZC1tZXJjaGFudEBnZXRicmFpbnRyZWUuY29tIiwic2NvcGVzIjoiYXV0aG9yaXphdGlvbnM6YnJhaW50cmVlIHVzZXIiLCJyZWRpcmVjdFVybCI6Imh0dHBzOi8vYXNzZXRzLmJyYWludHJlZWdhdGV3YXkuY29tL2NvaW5iYXNlL29hdXRoL3JlZGlyZWN0LWxhbmRpbmcuaHRtbCIsImVudmlyb25tZW50Ijoic2hhcmVkX3NhbmRib3gifSwibWVyY2hhbnRJZCI6ImRjcHNweTJicndkanIzcW4iLCJ2ZW5tbyI6Im9mZmxpbmUiLCJhcHBsZVBheSI6eyJzdGF0dXMiOiJtb2NrIiwiY291bnRyeUNvZGUiOiJVUyIsImN1cnJlbmN5Q29kZSI6IlVTRCIsIm1lcmNoYW50SWRlbnRpZmllciI6Im1lcmNoYW50LmNvbS5icmFpbnRyZWVwYXltZW50cy5zYW5kYm94LkJyYWludHJlZS1EZW1vIiwic3VwcG9ydGVkTmV0d29ya3MiOlsidmlzYSIsIm1hc3RlcmNhcmQiLCJhbWV4Il19fQ==", nil)
             
             DDLogError("Customer ID nil for Stripe Client.")
@@ -329,11 +332,65 @@ class BraintreeBackendAPI: NSObject, BraintreeBackendAPIAdapter {
         })
        
     }
+    //With String
+    func updateSourceForCustomerString(_ paymentMethod: String,
+                                 oldPaymentMethod: String,
+                                 completion: @escaping BTErrorBlock) {
+        
+        /*     guard let customerID = customerID else {
+         
+         let idx = self.sources.index(of: oldPaymentMethod)
+         if (idx != nil) {
+         
+         self.sources.remove(at: idx!)
+         self.sources.append(paymentMethod)
+         
+         // check if we removed the default card
+         if (self.defaultSource == paymentMethod) {
+         self.defaultSource = paymentMethod
+         }
+         }
+         
+         completion(nil)
+         
+         DDLogError("Customer ID nil for Stripe Client.")
+         return;
+         }
+         
+         let path = "/customers/\(customerID)/sources"
+         
+         let params = [
+         "customer": customerID,
+         "source":   paymentMethod.nonce,
+         ]
+         
+         let client: BAAClient = BAAClient.shared()
+         client.postPath(path, parameters: params,
+         
+         success: {(responseObject: (Any?)) -> Void in
+         completion(nil)
+         },
+         
+         failure: {(error: (Error?)) -> Void in
+         completion(error as NSError?)
+         }
+         )*/
+        let client: BAAClient = BAAClient.shared()
+        client.updatePaymentMethod(BAASBOX_RIDER_STRING, paymentMethodToken: oldPaymentMethod, paymentMethodNonce: paymentMethod, completion: {(success, error) -> Void in
+            
+            print(success as Any)
+            
+            completion(error as NSError?)
+            
+        })
+        
+    }
+
     
     func deleteSourceFromCustomer(_ paymentMethod: BTPaymentMethodNonce,
                                         completion: @escaping BTErrorBlock) {
 
-        guard let customerID = customerID else {
+       /* guard let customerID = customerID else {
             
             
             let idx = self.sources.index(of: paymentMethod)
@@ -371,5 +428,66 @@ class BraintreeBackendAPI: NSObject, BraintreeBackendAPIAdapter {
                             completion(error as NSError?)
             }
         )
+    }*/
+        let client: BAAClient = BAAClient.shared()
+        client.deletePaymentMethod(BAASBOX_RIDER_STRING, paymentMethodToken: paymentMethod.nonce, completion: {(success, error) -> Void in
+            
+            print(success as Any)
+            
+            completion(error as NSError?)
+            
+        })
     }
+    func deleteSourceFromCustomerString(_ paymentMethod: String,
+                                  completion: @escaping BTErrorBlock) {
+        
+        /* guard let customerID = customerID else {
+         
+         
+         let idx = self.sources.index(of: paymentMethod)
+         if (idx != nil) {
+         
+         self.sources.remove(at: idx!)
+         
+         // check if we removed the default card
+         if (self.defaultSource == paymentMethod) {
+         self.defaultSource = self.sources.last
+         }
+         }
+         
+         completion(nil)
+         
+         DDLogError("Customer ID nil for Stripe Client.")
+         return;
+         }
+         
+         let path = "/customers/\(customerID)/sources"
+         
+         let params = [
+         "customer": customerID,
+         "source":   paymentMethod.nonce,
+         ]
+         
+         let client: BAAClient = BAAClient.shared()
+         client.deletePath(path, parameters: params,
+         
+         success: {(responseObject: (Any?)) -> Void in
+         completion(nil)
+         },
+         
+         failure: {(error: (Error?)) -> Void in
+         completion(error as NSError?)
+         }
+         )
+         }*/
+        let client: BAAClient = BAAClient.shared()
+        client.deletePaymentMethod(BAASBOX_RIDER_STRING, paymentMethodToken: paymentMethod, completion: {(success, error) -> Void in
+            
+            print(success as Any)
+            
+            completion(error as NSError?)
+            
+        })
+    }
+
 }
